e4ab3debdeb297bf17db3742decec57215187a38
Philip Robinson <prrobinson74@outlook.com>
2022-05-23 21:18:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,6 @@ streamlit.text("\N{cricket}  Skinny cricket fries")
 streamlit.text("\N{cockroach}  Cockroach crisps")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick something from the list", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
