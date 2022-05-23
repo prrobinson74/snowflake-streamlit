7c31f367c4209544292ffe7efa456a82a70eaa1a
Philip Robinson <prrobinson74@outlook.com>
2022-05-23 21:11:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit
 streamlit.title("My parents new insect grub")
 streamlit.header("grub grub menu")
 streamlit.text("\N{beetle} Omega 3 beetle muffin")
-streamlit.text("\N{cricket}  Skinny cricket crisps")
+streamlit.text("\N{cricket}  Skinny cricket fries")
+streamlit.text("\N{cockroach}  Cockroach crisps")
