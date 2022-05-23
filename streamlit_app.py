3caa9ecf90ff81a4c6f25fe0f602f26b37a0886f
Philip Robinson <prrobinson74@outlook.com>
2022-05-23 21:04:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title("My parents new healty diner")
 streamlit.header("Breakfast menu")
 streamlit.text("Omega 3 Blueberry muffin")
-streamlt.text("Skinny late")
+streamlit.text("Skinny late")
