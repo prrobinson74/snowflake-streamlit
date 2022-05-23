4509ae6f9bab4e406d97359042e579e0362f96ff
Philip Robinson <prrobinson74@outlook.com>
2022-05-23 21:14:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit
+import pandas
+
+# Unicode ref: https://unicode.org/emoji/charts/emoji-list.html#1f4aa
 
 streamlit.title("My parents new insect grub")
 streamlit.header("grub grub menu")
 streamlit.text("\N{beetle} Omega 3 beetle muffin")
 streamlit.text("\N{cricket}  Skinny cricket fries")
 streamlit.text("\N{cockroach}  Cockroach crisps")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
